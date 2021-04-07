35d7c9b9f9bbafe1c4d8ab023ac35ac7bceb3fdd
Enes Yilmaz <enesyilmaz60@hotmail.com>
2021-04-07 10:22:48 +0200

    dotenv node fetch usw.

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const fetch = require('node-fetch')
+const PORT = process.env.PORT || 5000
+const app = express()
+require('dotenv').config()
+
+app.listen(PORT, () => {
+  console.log(`listening at ${PORT}`);
+})
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.MY_API_KEY}`)
+    .then(result => result.json())
+    .then(data => {
+      // console.log(data)
+      // res.json : Good for testing
+      // res.json(data)
+      // console.log(data.articles[0]);
+      res.render('pages/index', { data: data.articles[0], cool: process.env.SECRET })
+    })
+})
+
+
